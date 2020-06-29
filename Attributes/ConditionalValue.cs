7465fdc9448c4dc9ec3b2d6667cb65d0f1b40351
anloffredo <anloffredo@massageenvy.com>
2020-06-29 09:38:53 -0700

    Auto Commit

---
+++ Attributes/ConditionalValue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Penguin.Json.Abstractions.Attributes
+{
+    public abstract class ConditionalValue
+    {
+        protected object ovalue;
+
+        private bool shouldSerialize = true;
+
+        public Type ObjectType { get; private set; }
+
+        public bool ShouldSerialize
+        {
+            get
+            {
+                return ShouldSerializeFunc?.Invoke() ?? shouldSerialize;
+            }
+            set
+            {
+                shouldSerialize = value;
+            }
+        }
+
+        protected Func<object> GetValueFunc { get; set; }
+
+        protected Func<bool> ShouldSerializeFunc { get; set; }
+
+        public ConditionalValue(Type objectType)
+        {
+            ObjectType = objectType;
+        }
+
+        public T GetValue<T>() => (T)ovalue;
+
+        public object GetValue() => GetValueFunc?.Invoke() ?? ovalue;
+
+        public override string ToString()
+        {
+            return ovalue?.ToString();
+        }
+    }
+}
---
+++ Attributes/ConditionalValueT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Penguin.Json.Abstractions.Attributes
+{
+    public class ConditionalValue<T> : ConditionalValue
+    {
+        public T Value
+        {
+            get => GetValue<T>();
+            set => ovalue = value;
+        }
+
+        public ConditionalValue(T value, Func<bool> shouldSerializeFunc) : base(typeof(T))
+        {
+            Value = value;
+            ShouldSerializeFunc = shouldSerializeFunc;
+        }
+
+        public ConditionalValue(T value) : this(value, null)
+        {
+        }
+
+        public ConditionalValue(Func<bool> shouldSerializeFunc = null) : base(typeof(T))
+        {
+            ShouldSerializeFunc = shouldSerializeFunc;
+        }
+
+        public ConditionalValue(Func<T> getValue, Func<bool> shouldSerializeFunc = null) : this(shouldSerializeFunc)
+        {
+            ShouldSerializeFunc = shouldSerializeFunc;
+            GetValueFunc = () => getValue;
+        }
+
+        public static implicit operator ConditionalValue<T>(T b) => new ConditionalValue<T>(b);
+
+        public static implicit operator T(ConditionalValue<T> d) => d.Value;
+    }
+
+}
